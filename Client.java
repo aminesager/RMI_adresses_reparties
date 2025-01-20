2eba98b303d4e20d7e6f61004603a65b2ff22254
aminesager <amine.saguer1@gmail.com>
2025-01-20 18:15:50 +0100

    l'ajout du authentification

--- Client.java
+++ Client.java
@@ -1,35 +1,66 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.rmi.registry.*;
 import java.util.List;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("donner le mot de passe: ");
+            String inputPassword = scanner.nextLine();
+            if (!ServeurCarnetAdresses.auth(inputPassword)) {
+                System.out.println("Mot de passe incorrect !");
+                return;
+            }
             Registry registry = LocateRegistry.getRegistry("localhost");
-            CarnetAdresses carnetAdresses = (CarnetAdresses) registry.lookup("Adresses");
+            CarnetAdresses CarnetAdressesService = (CarnetAdresses) registry.lookup("Adresses");
             List<String> contactsList;
+            
 
-            carnetAdresses.ajouterContact("ali", "1234");
-            carnetAdresses.ajouterContact("iheb", "4321");
 
-            System.out.println("Recherche ali: " + carnetAdresses.rechercherContact("ali"));
-
-            carnetAdresses.supprimerContact("ali");
-
-            contactsList = carnetAdresses.listerContacts();
+            
+            CarnetAdressesService.supprimerContact("ali");
+            contactsList = CarnetAdressesService.listerContacts();
+            System.out.println("avant de modifier");
             System.out.println("Contacts restants: " + contactsList);
 
-            carnetAdresses.modifierContact("iheb", "9999");
+            CarnetAdressesService.modifierContact("iheb", "9999");
+            
 
-            System.out.println("Recherche mohamed amine: " + carnetAdresses.rechercherContact("mohamed amine"));
+            contactsList = CarnetAdressesService.listerContacts();
+            System.out.println("apres de modifier");
+            System.out.println("Contacts restants: " + contactsList);
 
-            contactsList = carnetAdresses.listerContacts();
-            System.out.println("Contacts restants apres modification: " + contactsList);
+            CarnetAdressesService.ajouterContact("mohamed amine", "7777" );
+            CarnetAdressesService.ajouterContact("mohamed ali ", "8888" );
+            CarnetAdressesService.ajouterContact("mohamed bechir", "6666" );
+            CarnetAdressesService.ajouterContact("examen tp reparties", "94194" );
 
-            carnetAdresses.ajouterContact("mohamed amine", "7777");
-            carnetAdresses.ajouterContact("mohamed ali", "8888");
-            carnetAdresses.ajouterContact("mohamed bechir", "6666");
-            carnetAdresses.ajouterContact("examen tp reparties", "5555");
-            carnetAdresses.ajouterContact("distributed systems", "1919");
+            contactsList = CarnetAdressesService.listerContacts();
+            try {
+                File Obj = new File("myfile.txt");
+                FileWriter Writer = new FileWriter("myfile.txt", true);
+                for (int i = 0 ; i < contactsList.size(); i++) {
+                    Writer.write(contactsList.get(i) + "\n");
+                } 
+                Writer.close();
+                System.err.println("le contenu du fichier: ");
+                Scanner Reader = new Scanner(Obj);
+                while (Reader.hasNextLine()) {
+                    String data = Reader.nextLine();
+                    System.out.println(data);
+                }
+                Reader.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+
+
 
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
--- ServeurCarnetAdresses.java
+++ ServeurCarnetAdresses.java
@@ -2,17 +2,25 @@ import java.rmi.registry.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 
-public class ServeurCarnetAdresses {
+public class ServeurCarnetAdresses  {
+    HashMap<String, String> Adresses = new HashMap<>();
+    private static final String PSW = "say my name";
+    CarnetAdressesImpl carnet = new CarnetAdressesImpl(Adresses);
+
     public static void main(String[] args) {
         try {
-            CarnetAdressesImpl carnetAdressesService = new CarnetAdressesImpl(new HashMap<>());
-            CarnetAdresses carnetAdresses = (CarnetAdresses) UnicastRemoteObject.exportObject(carnetAdressesService, 0);
+            CarnetAdressesImpl CarnetAdressesImpl = new CarnetAdressesImpl(new HashMap<>());
+            CarnetAdresses CarnetAdressesService = (CarnetAdresses) UnicastRemoteObject.exportObject(CarnetAdressesImpl, 0);
             Registry registre = LocateRegistry.createRegistry(1099); // Create registry if not existing
-            registre.bind("Adresses", carnetAdresses);
-            System.err.println("Le serveur est en marche");
+            registre.bind("Adresses", CarnetAdressesService);
+            System.err.println("Serveur ok");
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
         }
+    }
+    
+    public static boolean auth(String password) {
+        return PSW.equals(password);
     }
 }
